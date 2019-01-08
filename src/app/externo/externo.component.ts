268603b0d6347c8ab7177f8d7f523a7499a54183
Jose Carlos Calzada <josecarlosplatos@gmail.com>
2019-01-08 21:18:24 +0100

    Terminados ejercicios con Angular

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -11,11 +11,16 @@ export class ExternoComponent implements OnInit {
   public usuario: any;
   public idUsuario: any;
   public fecha: any;
+  public new_user: any;
+  public usuario_guardado: any;
+
   constructor(
     private _peticionesService: PeticionesService
   ) {
     this.usuario ="";
     this.idUsuario = 1;
+    this.new_user = { "name": "",
+                      "job": "" }
   }
 
   ngOnInit() {
@@ -35,6 +40,20 @@ export class ExternoComponent implements OnInit {
         this.usuario = "Error al cargar datos";
       }
     );
+  }
+
+  onSubmit(form){
+      this._peticionesService.addUser(this.new_user).subscribe(
+      response => {
+        console.log(response);
+        this.usuario_guardado = response;
+        form.reset();
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+    document.getElementById("name").focus();
   }
 
 }
--- src/app/service/peticiones.service.ts
+++ src/app/service/peticiones.service.ts
@@ -17,5 +17,12 @@ export class PeticionesService{
     return this._http.get(this.url + "api/users/" + idUsuario);
   }
 
+  addUser(user): Observable<any>
+  {
+    let params = JSON.stringify(user);
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this._http.post(this.url+'api/users', params, {headers: headers});
+  }
 
 }
